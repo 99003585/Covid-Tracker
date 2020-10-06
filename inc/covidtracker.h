f766385addc2fd30a5ede66a857076aacf9387db
Theres Mary Jose <theresmary98@gmail.com>
2020-10-07 01:14:14 +0530

    Paths added

---
+++ inc/covidtracker.h
@@ -0,0 +1,18 @@
+#ifndef COVIDTRACKER_H_INCLUDED
+#define COVIDTRACKER_H_INCLUDED
+
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<windows.h>
+
+void gotoxy(int ,int );
+void insert();
+void display();
+void search();
+void update();
+void delete();
+
+#endif // COVIDTRACKER_H_INCLUDED
