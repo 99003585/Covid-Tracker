33357b596c328363755a246866d17eaaf33cb1b9
Theres Mary Jose <theresmary98@gmail.com>
2020-10-07 12:11:30 +0530

    Deleted unity

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <covidtracker.h>
+#include <test_covidtracker.h>
 
 #include<stdio.h>
 #include<conio.h>
@@ -20,61 +21,13 @@ void main()
     gotoxy(15,8);
     printf("<------ COVID TRACKER SYSTEM ------>");
     menu();
+
+	/* Calling test main*/
+    
+    test_main();								/* Add function call */
+	return 0;
 }
 
 
-void menu()
-{
-    int choice;
-    system("cls");
-    gotoxy(10,3);
-    printf("<----- MENU ----->");
-    gotoxy(10,6);
-    printf("1 : Insert Record.");
-    gotoxy(10,7);
-    printf("2 : Display Record.");
-    gotoxy(10,8);
-    printf("3 : Search Record.");
-    gotoxy(10,9);
-    printf("4 : Update Record.");
-    gotoxy(10,10);
-    printf("5 : Delete Record.");
-    gotoxy(10,11);
-    printf("6 : Exit.");
-    gotoxy(10,14);
-    printf("Enter your choice.");
-    scanf("%d",&choice);
-
-    switch(choice)
-    {
-    case 1:
-        insert();
-        break;
-
-    case 2:
-        display();
-        break;
-
-    case 3:
-        search();
-        break;
-
-    case 4:
-        update();
-        break;
-
-    case 5:
-        delete();
-        break;
-
-    case 6:
-        exit(1);
-        break;
-
-    default:
-        gotoxy(10,17);
-        printf(" Invalid Option!!! Try again !!!\n");
-    }
-}
 
 
