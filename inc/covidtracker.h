c6e041f20b4581dc9906d6f0119cf70d278eaa59
Theres Mary Jose <theresmary98@gmail.com>
2020-10-09 11:17:30 +0530

    Documentation

--- inc/covidtracker.h
+++ inc/covidtracker.h
@@ -1,3 +1,9 @@
+/**
+* @file covidtracker.h
+
+*/
+
+
 #ifndef COVIDTRACKER_H_INCLUDED
 #define COVIDTRACKER_H_INCLUDED
 
@@ -6,10 +12,45 @@
 #include<string.h>
 #include<stdlib.h>
 
+
+/**
+* Tracks the latest covid-19 updates from different states of India.
+* @param[in] choice.
+* @return valid or invalid choice.
+* @note checks wheather the choice given by user is valid or not.
+*/
+int valid_choice(choice);
+
+
+/**
+* The user inputs the information such as the state name, active, recovered, deceased & other cases. 
+* @note Other cases include the migrated cases or non-COVID deaths.
+*/
 void insert();
+
+
+/**
+* All existing records are displayed along with state name, active, recovered, deceased & other cases. 
+*/
 void display();
+
+
+/**
+* State name is given as input to retrieve records of the respective state.
+*/
 void search();
+
+
+/**
+* This function is used to modify the existing records with latest updates of number of cases.
+*/
 void update();
+
+
+/**
+* This function is used to delete the existing record by entering the name of state.
+*/
 void delete();
+
 
 #endif // COVIDTRACKER_H_INCLUDED
