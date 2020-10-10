71bda320fe928b96c56fa1f2a5d055d338209233
stepin104890 <69132119+stepin104890@users.noreply.github.com>
2020-10-10 12:16:45 +0530

    Update covidtracker.c

--- Miniproject/Implementation/src/covidtracker.c
+++ Miniproject/Implementation/src/covidtracker.c
@@ -133,9 +133,9 @@ int display()
     fp = fopen("file.txt","rb+");
         if(fp == NULL)
         {
+              printf("");
+            //printf("\n\t\t\tNo Records Exists!...");
             return 1;
-            printf("\n\t\t\tNo Records Exists!...");
-            //return 1;
         }
     printf("\n\n\n\n\n\n\t\t\t<--:VIEW RECORD:-->");
     printf("\n\t\t\t\n\t\t\tS.No    STATE NAME            CONFIRMED    ACTIVE    RECOVERED    DECEASED    OTHERS");
